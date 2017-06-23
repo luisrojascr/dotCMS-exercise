81b95821708b928880852a09c115c40998564cce
Luis Rojas <luirojas3@crsjopgp0268.global.publicisgroupe.net>
2017-06-22 20:21:13 -0600

    First commit - Project starter

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NewsDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+  ],
+  providers: [
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
---
+++ src/app/news-detail/news-detail.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'; 
+
+@Component({ 
+  selector: 'news-detail',
+  styleUrls: ['./news-detail.component.css'],
+  templateUrl: './news-detail.component.html'
+})
+export class NewsDetailComponent {}
